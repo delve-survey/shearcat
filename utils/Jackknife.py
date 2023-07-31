f4bfe80040cd609944d8d67d23bdf8f46befd98e
Dhayaa Anbajagane <dhayaaanbajagane@gmail.com>
2023-07-31 02:04:45 -0500

    New example

--- utils/Jackknife.py
+++ utils/Jackknife.py
@@ -115,33 +115,56 @@ class Jackknife(object):
     
 if __name__ == '__main__':
     
+    from scipy.optimize import curve_fit
+    
     #Make some example data
-    
-    Ngal = 1_000_000
+    Ngal = 100_000
     ra, dec = np.random.uniform(30, 180, Ngal), np.random.uniform(-60, 30, Ngal)
-    e1, e2  = np.random.uniform(-1, 1, Ngal), np.random.uniform(-1, 1, Ngal)
-    
+    p1, p2  = np.random.uniform(-1, 1, Ngal), np.random.uniform(-1, 1, Ngal)
+    e1, e2  =  np.random.uniform(-1, 1, Ngal) + 0.3*p1, np.random.uniform(-1, 1, Ngal) + 0.3*p2 + 0.1*p1
     
     knife_it = Jackknife(ra, dec, 100)
     
     
+    #EXAMPLE 1: Compute variance of shape noise (its just a toy calculation)
     def bin_shape_variance(e1, e2):
         X = 1/2 * (e1**2 + e2**2)
-        return np.histogram(X, bins = np.linspace(0, 1, 10))[0]
+        return np.histogram(X, bins = np.linspace(0, 1, 4))[0]
     
     mean, cov, raw_output = knife_it.go(bin_shape_variance, e1 = e1, e2 = e2)
     
-    print("MEAN:", mean)
-    print("COV :", cov)
+    print("MEAN:", np.round(mean, 3))
+    print("COV :", np.round(cov, 3))
     
     
-    def bin_shape_average(e1, e2):
-        X = 1/2 * (e1 + e2)
-        return np.histogram(X, bins = np.linspace(0, 1, 10))[0]
+    #EXAMPLE 2: Computing quantity X vs, Y and fitting slope
+    def e1_vs_p1(e1, p1):
+        '''
+        Compute mean e1 in bins of p1
+        '''
+        
+        p1_bins = np.linspace(-1, 1, 11)
+        mean_p1 = (p1_bins[1:] + p1_bins[:-1])/2
+        mean_e1 = np.histogram(p1, bins = p1_bins, weights = e1)[0]/np.histogram(p1, bins = p1_bins)[0]
+        
+        return mean_e1
     
-    mean, cov, raw_output = knife_it.go(bin_shape_average, e1 = e1, e2 = e2)
+    mean, cov, raw_output = knife_it.go(e1_vs_p1, e1 = e1, p1 = p1)
     
-    print("MEAN:", mean)
-    print("COV :", cov)
+    print("MEAN:", np.round(mean, 3))
+    
+    #NOW FIT A LINE TO IT
+    def line(x, m, c):
+        return m * x + c
+    
+    p1_bins = np.linspace(-1, 1, 11)
+    mean_p1 = (p1_bins[1:] + p1_bins[:-1])/2
+
+    popt, cov = curve_fit(line, mean_p1, mean, sigma = cov)
+    m, c = popt
+
+    print("SLOPE:", np.round(m, 3))
+    print("SLOPE ERR:", np.round(np.sqrt(cov[0, 0]), 3))
+
     
     
