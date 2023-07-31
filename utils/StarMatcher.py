01f6d3b7d9498997054b01c9d6a70aa03c35179d
Dhayaa Anbajagane <dhayaaanbajagane@gmail.com>
2023-07-31 17:40:13 -0500

    Star Matching code update

--- utils/StarMatcher.py
+++ utils/StarMatcher.py
@@ -20,14 +20,14 @@ class StarMatch(object):
         self.UniquePix = output[0]
 
 
-    def match(self, nside = 8096*32):  
+    def match(self, nside = 8192*32):  
 
         hpix = hp.ang2pix(nside, self.ra, self.dec, lonlat = True)
 
         unique_hpix, inds, unique_counts = np.unique(hpix, return_inverse = True, return_counts = True)
 
         print("TOTAL:", len(hpix))
-        print("UNIQUE MATCHED:", len(hpix))
+        print("UNIQUE MATCHED:", len(unique_hpix))
         print("MAX REPEATS:", np.max(unique_counts))
         print("MIN REPEATS:", np.min(unique_counts))
 
@@ -37,12 +37,9 @@ class StarMatch(object):
     def weighted_average(self, A, weights = None):
 
         wgt = np.bincount(self.inds, weights = weights)
-        sm  = np.bincount(self.inds, weights = A*weights)
+        sm  = np.bincount(self.inds, weights = A*weights)        
         
-        
-        
-        avg = wgt/sm
-#         avg = avg[wgt != 0]
+        avg = sm/wgt
 
         return avg
 
@@ -51,52 +48,55 @@ if __name__ == '__main__':
 
     with h5py.File('/project/chihway/dhayaa/DECADE/star_psf_shapecatalog_20230510.hdf5', 'r') as f:
 
-        keys = ['ra', 'dec', 'FLUX_AUTO',
-                'g1_star_hsm', 'g2_star_hsm', 'T_star_hsm', 
-                'g1_model_hsm','g2_model_hsm','T_model_hsm', 
-                'IMAFLAGS_ISO']
-
-        N = 1_000_000_000_000
+        ra   = np.array(f['ra']).astype(np.float32)
+        dec  = np.array(f['dec']).astype(np.float32)
+        
+        hpix = hp.ang2pix(8192 * 32, ra, dec, lonlat = True)
+        unique_hpix, inds, unique_counts = np.unique(hpix, return_inverse = True, return_counts = True)
+        
+        out  = np.zeros([3, hpix.size], dtype = np.float32) + np.NaN
+        
+        del hpix, unique_counts
         
         #Get selections for individual bands first
-        BAND = np.array(f['BAND'][:N]).astype('U1')
-
-        Masks    = [np.where(BAND == 'r')[0], np.where(BAND == 'i')[0], np.where(BAND == 'z')[0]]
-        Catalogs = []
-
-        ra  = np.array(f['ra'][:N])
-        dec = np.array(f['dec'][:N])
-
-        SNR = np.array(f['FLUX_AUTO'][:N])/np.array(f['FLUXERR_AUTO'][:N])
+        BAND  = np.array(f['BAND']).astype('U1')
+        Masks = [np.where(BAND == 'r')[0], np.where(BAND == 'i')[0], np.where(BAND == 'z')[0]]
+        
+        flux = np.array(f['FLUX_AUTO'])
+        SNR  = flux/np.array(f['FLUXERR_AUTO'])
+        ZP   = np.array(f['MAGZP'])
+        mag  = ZP - 2.5 * np.log10(flux)
+        
+        del flux, ZP, BAND
+        
+        #This helps avoid nan errors later down the line
+        #We set the mag to 0 but we don't really use it 
+        #since the SNR will also be zero
+        SNR  = np.where(SNR < 0, 0, SNR)
+        mag  = np.where(np.isfinite(mag), mag, 0)
         
         print("LOADED RA, DEC, SNR")
         
-        for m in Masks:
+        for i in tqdm(range(len(Masks)), desc = 'Matching bands'):
+            
+            m = Masks[i]
 
-            Matcher = StarMatch(ra = ra[m],  dec = dec[m])
-
-            out = pd.DataFrame()
-
-            out['inds'] = Matcher.UniquePix #The healpix pixel this star belongs to
-
-            for k in keys:
-
-                out[k] = Matcher.weighted_average(f[k][:N][m], weights = SNR[m]) #Average all quantities together
+            Band_Matcher = StarMatch(ra = ra[m],  dec = dec[m])
+            weighted_mag = Band_Matcher.weighted_average(mag[m], weights = SNR[m]) #Average all quantities together
                 
-            out['FLUXERR_AUTO'] = np.sqrt(Matcher.weighted_average(f['FLUXERR_AUTO'][:N][m]**2, weights = SNR[m]))
+            Masks[i] = None #This just "deletes" the mask in here.
+            
+            weighted_mag_map    = np.zeros(unique_hpix.size) + np.NaN
+            tmp, inds_a, inds_b = np.intersect1d(unique_hpix, Band_Matcher.UniquePix, return_indices = True)
+            
+            print("Matched index size:", len(tmp))
+            
+            weighted_mag_map[inds_a] = weighted_mag[inds_b]; del weighted_mag
+           
+            out[i, :] = weighted_mag_map[inds]; del weighted_mag_map
+            
+            
 
-            Catalogs.append(out)
-        
-    del Matcher, out, ra, dec, SNR, Masks, BAND
+    del Band_Matcher, ra, dec, SNR, Masks
 
-    #Combine all bands
-    Catalog = Catalogs[0]
-    Catalog = Catalog.join(Catalogs[1], on = 'inds', how = 'inner', lsuffix = 'r', rsuffix = 'i')
-    Catalog = Catalog.join(Catalogs[2], on = 'inds', how = 'inner', lsuffix = '',  rsuffix = 'z')
-
-    
-    with h5py.File('/project/chihway/dhayaa/DECADE/matched_star_psf_shapecatalog_20230630.hdf', 'w') as f:
-        
-        for k in keys + ['inds', 'FLUXERR_AUTO']:
-            f.create_dataset(k, data = Catalog[k])
-    
+    np.save('/project/chihway/dhayaa/DECADE/matched_star_psf_shapecatalog_20230630.npy', out)
