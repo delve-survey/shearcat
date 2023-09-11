2e12e0acf7fd4b9e2fe6c57440c36e0d0f67c6e3
Dhayaa Anbajagane <dhayaaanbajagane@gmail.com>
2023-09-11 14:43:24 -0500

    MAke Master PSF catalog

--- shear_rowe_stats/runs/Combine_catalogs_V2.py
+++ shear_rowe_stats/runs/Combine_catalogs_V2.py
@@ -1,4 +1,3 @@
-#use this to measure the stars' and psf models' ellipticities
 
 import numpy as np
 import fitsio
@@ -13,29 +12,58 @@ from tqdm import tqdm
 if __name__ == '__main__':
     
     
-    path = os.environ['ROWE_STATS_DIR']
-    
     #Get all files available
-    filelist = glob.glob(path + '/*.fits')
+    filelist1 = glob.glob('/project/chihway/dhayaa/DECADE/rowe_stats/DR3_1_1/output*.fits')
+    filelist2 = glob.glob('/project/chihway/dhayaa/DECADE/rowe_stats/DR3_1_2/output*.fits')
+    filelist3 = glob.glob('/project/chihway/dhayaa/DECADE/rowe_stats/DR3_1_2_decn30/output*.fits')
+    
+    filelist = filelist1 + filelist2 + filelist3 #Concatenate the lists
     filelist = sorted(filelist)
-        
+    
     #Open just one file to get column names
     columns = fitsio.FITS(filelist[0])[1].get_colnames()
     
-#     datasets = [fitsio.FITS(f) for f in tqdm(filelist, desc = 'filelist')]
-        
     def get_column(filelist, column):
         
         return np.concatenate([fitsio.FITS(d)[1].read(columns=column) for d in tqdm(filelist, desc = column[5:])], axis = 0)
     
     
-    with h5py.File('/scratch/midway2/dhayaa/test.hdf5', "w") as f:
-#     with h5py.File(os.path.join(path, 'star_psf_shapecat.hdf5'), "w") as f:
+    def mask(filelist):
+        
+        M = []
+#         EXCLUDE = fitsio.FITS('/home/dhayaa/Desktop/DECADE/shearcat/shear_rowe_stats/delve_exclude_20230725.fits')[1].read()
+        INCLUDE = fitsio.FITS('/home/dhayaa/Desktop/DECADE/FinalExpList.fits')[1].read()
+        INCLUDE_UNIQUE_EXP = np.unique(INCLUDE['EXPNUM'])
+        for f in tqdm(filelist, desc = "BUILD INCLUDE LIST  (20230910)"):
+            
+            CCD = fitsio.FITS(f)[1].read(columns='col0.EXPNUM')
+            EXP = fitsio.FITS(f)[1].read(columns='col0.CCDNUM')
+            
+            #Find all images where EXPnum is in include list
+            E = np.in1d(EXP, INCLUDE_UNIQUE_EXP)
+            
+            #For all exposures that were selected above, find the corresponding CCDs to include and now
+            #check if those CCD nums are in the catalog
+            C = np.in1d(CCD, INCLUDE['CCDNUM'][np.in1d(INCLUDE['EXPNUM'], np.unique(EXP[E]))])
 
+            #Final
+            T = C & E
+            
+            #Only select objects that ARE in the above
+            M.append(T)
+#             M.append(np.invert(T))
+            
+        return np.concatenate(M) 
+            
+    
+    with h5py.File('/scratch/midway3/dhayaa/star_psf_shapecat_20230910.hdf5', "w") as f:
+
+        MASK = mask(filelist)
+        
         #Create all columns you need
         for c in columns:
             if 'BAND' in c:
-                f.create_dataset(c[5:], data = get_column(filelist, c).astype('S'), dtype = h5py.special_dtype(vlen=str))
+                f.create_dataset(c[5:], data = get_column(filelist, c).astype('S')[MASK], dtype = h5py.special_dtype(vlen=str))
             else:
-                f.create_dataset(c[5:], data = get_column(filelist, c))
+                f.create_dataset(c[5:], data = get_column(filelist, c)[MASK])
             
