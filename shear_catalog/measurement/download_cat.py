7a9e48778a45e8d0b236fe840eb91f1f7f0f56f0
Chihway Chang <chihway@midway3-login4.rcc.local>
2024-05-03 09:36:21 -0500

    update

--- shear_catalog/measurement/download_cat.py
+++ shear_catalog/measurement/download_cat.py
@@ -4,24 +4,22 @@ import os
 import sys
 
 i = int(sys.argv[1])
+meta = sys.argv[2]
 
-metadata = np.genfromtxt('/home/chihway/shearcat/Tilelist_DR3_1_1.csv', dtype='str', delimiter=",")[1:][i]
+metadata = np.genfromtxt(meta, dtype='str', delimiter=",")[1:][i]
 
 tile = metadata[0]
-path = 'DEC_Taiga'+metadata[1][3:-5]
+path = 'DEC'+metadata[1][3:-5]
 
-
-#metadata = np.genfromtxt('tile_DR3_1_1.csv', dtype='str', delimiter=",")[1:][i]
-
-#tile = metadata[0][2:-1]
-#path = metadata[1][2:-1]
 
 command1 = 'wget --user=decade --password=decaFil3s  --recursive --no-parent --auth-no-challenge  https://decade.ncsa.illinois.edu/deca_archive/'+path+'/cat/'
 
-command2 = 'wget --user=decade --password=decaFil3s  --recursive --no-parent --auth-no-challenge  https://decade.ncsa.illinois.edu/deca_archive/'+path+'/wavg/'
+#command2 = 'wget --user=decade --password=decaFil3s  --recursive --no-parent --auth-no-challenge  https://decade.ncsa.illinois.edu/deca_archive/'+path+'/wavg/'
 
+#command2 = 'wget --user=decade --password=decaFil3s  --recursive --no-parent --auth-no-challenge  https://decade.ncsa.illinois.edu/deca_archive/DEC_Taiga/multiepoch/shear_SOF_DR3/r6015/'+tile+'/p01/sof/'
+command2 = 'wget --user=decade --password=decaFil3s  --recursive --no-parent --auth-no-challenge  https://decade.ncsa.illinois.edu/deca_archive/DEC_Taiga/multiepoch/shear_SOF_DR3/r6128/'+tile+'/p01/sof/'
 
 os.system(command1)
 os.system(command2)
-
+#os.system(command3)
 
